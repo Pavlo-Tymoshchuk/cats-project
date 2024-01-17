b5b99a8fcc9ff94f8a8ede8725d7d668374c48f3
pavel-tymoshchuk <pavel-tymoshchuk@uadreams.com>
2024-01-17 09:48:09 +0200

    created independent cats list component

--- src/pages/RandomCats/index.tsx
+++ src/pages/RandomCats/index.tsx
@@ -1,43 +1,11 @@
 import { FC } from 'react'
-import classes from './styles/randomCats.module.scss'
 
-import CatCard from '../../components/CatCard'
-import { Alert } from 'react-bootstrap'
-import Button from '../../components/Button'
-import CatModal from '../../components/CatModal'
-
-import { useGetRandomCats } from '../../hooks/useGetRandomCats'
-
-import { createCatTitle } from '../../utils/helpers'
+import CatsList from './components/CatsList'
 
 const RandomCats: FC = () => {
-    const { canLoadMore, globalError, loading, catsList, loadMoreCats } = useGetRandomCats()
-
-    if (loading && !catsList?.length) {
-        return 'loading...'
-    }
-
-    if (!catsList?.length || globalError) {
-        return <Alert variant="danger">{globalError || 'Cats not found.'}</Alert>
-    }
-
     return (
         <>
-            <section className={classes['random-cats']}>
-                <ul className={classes['random-cats-list']}>
-                    {catsList.map((item) => (
-                        <CatCard imageUrl={item.url} name={createCatTitle(item?.breeds)} catId={item.id} key={item.id} />
-                    ))}
-                </ul>
-                {canLoadMore ? (
-                    <div className={classes['random-cats-more']}>
-                        <Button type="primary" onClick={loadMoreCats} loading={loading}>
-                            Load more cats
-                        </Button>
-                    </div>
-                ) : null}
-            </section>
-            <CatModal />
+            <CatsList />
         </>
     )
 }
